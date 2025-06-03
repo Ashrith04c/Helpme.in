26cf564c459531d4c6cd2a5be611134692c607ee
Ashrith04c <ashrith.04c@gmail.com>
2025-06-03 16:57:11 +0530

    Welcomepage

--- src/components/Log.jsx
+++ src/components/Log.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import './wel.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 const Wel = () => {
   return (
     <div className="welcome-container">
-      <h1>Hello</h1>
-      <h2>Ambur</h2>
+      <h1 className="typing-text">Hello</h1>
+      <h2 className="typing-delay"> Ambur</h2>
       <button className="circle-button">
-        &gt;
+       <i className="fa-solid fa-chevron-right"></i>
       </button>
       <p className="get-started">Get Started</p>
     </div>
